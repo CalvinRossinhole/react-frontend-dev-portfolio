51131341c14378dd486227e22eeda7768b21fdcc
Calvin Feitosa Rossinhole <calvin.masters@gmail.com>
2022-03-30 00:11:41 -0300

    Updates

---
+++ precache-manifest.601fd1241409b74020b5f6b68ef989ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946cf2506cd5875558a45e10e213b9fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf389746efb26f40cd91",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "a9b625f5ca6ea39d4224",
+    "url": "/static/css/main.3655595c.chunk.css"
+  },
+  {
+    "revision": "cf389746efb26f40cd91",
+    "url": "/static/js/2.c82ea141.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.c82ea141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b625f5ca6ea39d4224",
+    "url": "/static/js/main.8442510c.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
